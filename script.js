ba146a97b241577e3377c6bf4892a24519466f27
JUHI NAGPURE <juhinagpure143@gmail.com>
2022-09-27 22:03:33 +0530

    added js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-document.getElementById("count-el").innerText = 5
+// document.getElementById("count-el").innerText = 5
+
+// cmd+k+c
+//let firstBatch = 5
+//let secondBatch = 7
+//let count = firstBatch + secondBatch
+//console.log(count)
+
+//1.Create two variables,myAge and
+
+
+
+
+let myAge = 35
+let humanDogRatio = 7
+
+let myDogAge = myAge * humanDogRatio
+
+console.log(myDogAge)
